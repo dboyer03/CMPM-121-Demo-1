fe6890267c485751267667a83a45cded8dfb230b
Dylan <dylanjboyer@gmail.com>
2024-10-06 14:43:27 -0700

    initial commit - game title change

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "a very original game title";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+
