925b758cc2bbb423861df74f08065f18d92c1cbf
Dylan <59846003+dboyer03@users.noreply.github.com>
2024-10-07 16:27:05 -0700

    Step 1: added a button

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "a very original game title";
+// set the title of the page
+const gameName = "macdonald fry counter game";
 document.title = gameName;
 
+// add a header
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// add a button
+const buttonName = "🍟";
+
+const button = document.createElement("button");
+button.innerHTML = buttonName;
+app.append(button);
+
 
 
